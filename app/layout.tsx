0136f100f4d39b741532ee213e367127c2d081c1
zhh312 <fjzhenghonghao@gmail.com>
2024-04-07 19:56:11 -0400

    name

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Navigation from "@/components/Navigation";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Honghao's Portfolio",
+  title: "Ryan's Portfolio",
   description: "This is my portfolio.",
 };
 
